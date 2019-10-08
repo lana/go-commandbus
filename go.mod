0f1195db5cb322c2308b11227e98daef05363b87
Fabio Ribeiro <faabiosr@gmail.com>
2019-10-08 14:59:55 -0300

    Dispatcher Middleware
    This new middleware was created to dispatch event when a command
    handler is executed.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vsmoraes/go-commandbus
 
 go 1.13
+
+require github.com/vsmoraes/go-dispatcher v0.0.1
---
+++ middleware/dispatcher.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/vsmoraes/go-commandbus"
+	"github.com/vsmoraes/go-dispatcher"
+)
+
+const (
+	DispatcherReceived = "received"
+	DispatcherExecuted = "executed"
+)
+
+type event struct {
+	name string
+	cmd  interface{}
+}
+
+func (e event) Type() dispatcher.EventType {
+	name := reflect.TypeOf(e.cmd).Elem().Name()
+	name = fmt.Sprintf("%s.%s", strings.ToLower(name), e.name)
+
+	return dispatcher.EventType(name)
+}
+
+func (e event) Data() interface{} {
+	return e.cmd
+}
+
+// Dispatcher will dispatch events when a command was received and executed.
+func Dispatcher(d dispatcher.Dispatcher) commandbus.MiddlewareFunc {
+	return func(next commandbus.HandlerFunc) commandbus.HandlerFunc {
+		return func(ctx context.Context, cmd interface{}) error {
+			d.Dispatch(ctx, event{DispatcherReceived, cmd})
+
+			if err := next(ctx, cmd); err != nil {
+				return err
+			}
+
+			d.Dispatch(ctx, event{DispatcherExecuted, cmd})
+
+			return nil
+		}
+	}
+}
