383a2a7b81a09e5d3b6de97d257dee073658485a
Fabio Ribeiro <faabiosr@gmail.com>
2019-10-13 08:39:00 -0300

    Create Logger tests
    Created tests for Logger middleware.

--- middleware/dispatcher_test.go
+++ middleware/dispatcher_test.go
@@ -2,16 +2,11 @@ package middleware
 
 import (
 	"context"
-	"errors"
 	"reflect"
 	"testing"
 
 	"github.com/vsmoraes/go-dispatcher"
 )
-
-type CreateUser struct {
-	Err bool
-}
 
 func TestDispatcher(t *testing.T) {
 	d := dispatcher.New()
@@ -23,16 +18,6 @@ func TestDispatcher(t *testing.T) {
 
 	mw := Dispatcher(d)
 	ctx := context.Background()
-
-	next := func(ctx context.Context, cmd interface{}) error {
-		v := cmd.(*CreateUser)
-
-		if v.Err {
-			return errors.New("command was failed")
-		}
-
-		return nil
-	}
 
 	if err := mw(next)(ctx, &CreateUser{true}); err == nil {
 		t.Errorf("failed to execute the middleware: expected an error and got %v", err)
